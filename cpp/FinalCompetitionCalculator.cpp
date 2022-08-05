1ad6f60e7036539abfaa2c2c4cf6c9483eb35e7e
TheSunspot <1092312830@qq.com>
2022-08-06 00:33:05 +0800

    1

--- cpp/FinalCompetitionCalculator.cpp
+++ cpp/FinalCompetitionCalculator.cpp
@@ -64,6 +64,12 @@ void FinalCompetitionCalculator::doCalculate(condition_variable &cv, unique_lock
     auto ed = std::chrono::steady_clock::now();
     std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(ed - be);
     cout << "calculate module time cost:" << time_span.count() << "s.\n";
+    cout << "----------------------------------------------\n";
+    cout << "chosen parms: Brand#" << this->targetBrand.first << this->targetBrand.second
+         << "  Container: " << this->targetContainer << endl;
+    cout << fixed << setprecision(10)
+         << "selected answer: "
+         << this->data.underSales / 7 << endl;
 }
 
 void FinalCompetitionCalculator::localCalculate()
@@ -172,6 +178,4 @@ void FinalCompetitionCalculator::mergeQueryAns(condition_variable &cv, unique_lo
     }
     doAsync(cv, lck);
     // this_thread::sleep_for(std::chrono::milliseconds(1000));
-    cout << fixed << setprecision(10)
-         << this->data.underSales / 7 << endl;
 }
